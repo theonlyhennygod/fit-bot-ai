6c96739c9853815cf062ec9b90f98731db5ef35a
Argenis De La Rosa <theonlyhennygod@gmail.com>
2024-11-26 03:40:47 -0500

    file restructured

--- file-saver.d.ts
+++
@@ -1,4 +0,0 @@
-// file-saver.d.ts
-declare module 'file-saver' {
-    export function saveAs(data: Blob | string, filename?: string, options?: any): void;
-  }
---
+++ types/file-saver.d.ts
@@ -0,0 +1,4 @@
+// types/file-saver.d.ts
+declare module 'file-saver' {
+  export function saveAs(data: Blob | string, filename?: string, options?: any): void;
+}
